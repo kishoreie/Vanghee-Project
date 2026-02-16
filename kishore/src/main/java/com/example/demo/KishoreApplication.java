87c186cd4030b564c65b22b3cb1d46ee61ba5f22
Vasanth Kumar <vasanthakumar.m@vanghee.com>
2026-02-16 17:14:12 +0530

    my new Project

---
+++ kishore/src/main/java/com/example/demo/KishoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KishoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KishoreApplication.class, args);
+	}
+
+}
---
+++ kishore/src/main/java/com/example/demo/entity/Demo.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "demo")
+public class Demo {
+	
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	@Column(name = "name")
+	private String name;
+	
+	
+	
+	
+	
+
+}
